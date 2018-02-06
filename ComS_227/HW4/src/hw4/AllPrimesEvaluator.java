45f1394fe7a875c3d8c867f7cd399c2eec0020a6
Eric Mullen <ericmullen1989@gmail.com>
2018-02-06 09:53:13 -0600

    Add files via upload

---
+++ ComS_227/HW4/src/hw4/AllPrimesEvaluator.java
@@ -0,0 +1,26 @@
+package hw4;
+
+/**
+ * Evaluator for a hand in which the rank of each card is a prime number.
+ * The number of main cards required is equal to the total cards.
+ * 
+ * The name of this evaluator is "All Primes".
+ */
+//Note: You must edit this declaration to extend AbstractEvaluator
+//or to extend some other class that extends AbstractEvaluator
+public class AllPrimesEvaluator
+{
+  /**
+   * Constructs the evaluator.
+   * @param ranking
+   *   ranking of this type of hand
+   * @param handSize
+   *   total number of cards in a hand
+   */
+  public AllPrimesEvaluator(int ranking, int handSize)
+  {
+    // TODO: call appropriate superclass constructor and 
+    // perform other initialization
+  }
+  
+}
