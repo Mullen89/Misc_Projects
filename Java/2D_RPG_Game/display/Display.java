dde0a34c86a0344200ac906579adcf396e26d3b5
Eric Mullen <ericmullen1989@gmail.com>
2021-11-01 17:53:30 -0500

    App (feat): Initial structure
    added initial structure and main function to run the display canvas (JFrame).

---
+++ Java/2D_RPG_Game/display/Display.java
@@ -0,0 +1,29 @@
+package display;
+
+import javax.swing.JFrame;
+
+public class Display {
+
+    private JFrame frame;
+    private String title;
+    private int height;
+    private int width;
+
+    public Display(String title, int height, int width) {
+        this.title = title;
+        this.height = height;
+        this.width = width;
+
+        createDisplay();
+    }
+
+    private void createDisplay() {
+        frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+}
---
+++ Java/2D_RPG_Game/main/Launcher.java
@@ -0,0 +1,10 @@
+package main;
+
+import display.Display;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Display d = new Display("This is the title", 800, 1000);
+
+    }
+}
